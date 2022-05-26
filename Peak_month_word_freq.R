4d2cfdeb7e6b8a4d73dd05c335c6d81032411656
Kang <kangjf1943@gmail.com>
2022-05-26 18:13:26 +0200

    feat: 添加2020年暖冬时段词频生成代码

--- Peak_month_word_freq.R
+++ Peak_month_word_freq.R
@@ -5,7 +5,21 @@ library(ggplot2)
 library(showtext)
 showtext_auto()
 
-# Function ----
+# Data ----
+# 用户自定义词典
+userdic <- "RawData/Global_warming_tweet.dic"
+
+# 设置停止词
+stopwords <- c(
+  "する","それ","なる","ない","そこ","これ","ある", "さん", "なん", "の", "ん", 
+  "いる", "思う", "そう", "れる", "くる", "考える", "言う", "ー", 
+  "できる", "てる", "でる", "世界の財界", "一", "いい", "何", "いう", "できる", 
+  "られる", "n", "RT", letters, LETTERS, 
+  "+", "<", ">", "><", "!!", "#", "!?", "@", "(", "/", "\\"
+)
+
+# Analysis ----
+## Get peak month word freq *.csv and plot ---- 
 # 函数：基于文本*.csv文件，生成文本*.txt文件，再解析并写出词频*.csv文件
 # 参数：
 # dir.csv：文本*.csv文件路径及文件名称
@@ -47,21 +61,7 @@ Csv2WfCsv <- function(dir.csv, name.tar, dir.dic, stopword) {
   }
 }
 
-# Data ----
-# 用户自定义词典
-userdic <- "RawData/Global_warming_tweet.dic"
-
-# 设置停止词
-stopwords <- c(
-  "する","それ","なる","ない","そこ","これ","ある", "さん", "なん", "の", "ん", 
-  "いる", "思う", "そう", "れる", "くる", "考える", "言う", "ー", 
-  "できる", "てる", "でる", "世界の財界", "一", "いい", "何", "いう", "できる", 
-  "られる", "n", "RT", letters, LETTERS, 
-  "+", "<", ">", "><", "!!", "#", "!?", "@", "(", "/", "\\"
-)
-
-# Analysis ----
-# 
+# 写出词频*.csv文件
 Csv2WfCsv(dir.csv = "RawData/ForKansaiConf/test_Clr202001lite.csv", 
           name.tar = "202001", dir.dic = userdic, stopword = stopwords)
 Csv2WfCsv(dir.csv = "RawData/ForKansaiConf/test_Clr202110lite.csv", 
@@ -98,3 +98,42 @@ PlotWf(name.tar = "202111", name.keyword = "gw")
 dev.off()
 PlotWf(name.tar = "202111", name.keyword = "cc")
 dev.off()
+
+## Get 2020 warm winter word freq *.csv ----
+# 目标日期字符串
+tar.date <- c(paste0(rep("2020-01-0", 5), 5:9), 
+              paste0(rep("2020-01-", 6), 10:15))
+
+# 构建目标数据集：2020年1月5日-15日的数据
+text.202001.part <- read.csv("RawData/2020/jst202001lite.csv") %>% 
+  as_tibble() %>% 
+  mutate(date = substr(created_at, 1, 10)) %>% 
+  subset(date %in% tar.date)
+
+# 将目标数据根据提及“”和“”进行分类，并且写出推文文本为*.txt文件
+text.202001.part %>% 
+  subset(grepl("温暖化", text)) %>% 
+  .$text %>% 
+  write(file = "ProcData/ForKansaiConf/Text_202001_part_gw.txt")
+text.202001.part %>% 
+  subset(grepl("気候変動", text)) %>% 
+  .$text %>% 
+  write(file = "ProcData/ForKansaiConf/Text_202001_part_cc.txt")
+
+# 读取*.txt文件并进行分词
+Wf2020JanPart <- function(name.keyword) {
+  docDF(target = paste0("ProcData/ForKansaiConf/Text_202001_part_", 
+                        name.keyword, ".txt"), 
+        type = 1, dic = userdic) %>% 
+    rename_with(~ c("term", "pos1", "pos2", "freq")) %>% 
+    subset(
+      pos1 %in% c("名詞","動詞","形容詞") & 
+        !(pos2 %in% c("非自立","接尾", "数")) &
+        !(term %in% stopwords)
+    ) %>% 
+    arrange(-freq) %>% 
+    write.csv(file = paste0("ProcData/ForKansaiConf/Wf_202001_part_", 
+                            name.keyword, ".csv"))
+}
+Wf2020JanPart("cc")
+Wf2020JanPart("gw")
